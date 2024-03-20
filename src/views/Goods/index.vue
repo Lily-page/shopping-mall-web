a366f8c92ff4ee638a0fc6feb1825fbbaf0e5572
dot <747595722@qq.com>
2024-03-20 17:25:47 +0800

    public分支重新打包

--- src/views/Goods/index.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <div>全部商品</div>
-</template>
--- src/views/PersonalCenter/OrderList/order-empty.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="m-exception m-no-order">
-    <div class="e-img"></div>
-    <p class="e-info">没有相应的订单数据</p>
-  </div>
-</template>
-<style lang="scss" scoped>
-@import '../style/order-list.scss';
-</style>
